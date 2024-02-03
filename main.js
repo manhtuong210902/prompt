b2a294b56f8035d2acd8d9da975f7fc4d6d0f814
manhtuong <mtuong669@gmail.com>
2024-02-03 23:00:10 +0700

    dev-feat: add new function

--- main.js
+++ main.js
@@ -21,3 +21,19 @@ function checkIfPrime(number) {
     }
     return true;
 }
+
+function inefficientFunction(arr) {
+    let sum = 0;
+    let oddCount = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+
+        if (arr[i] % 2 !== 0) {
+            oddCount++;
+        }
+    }
+
+    console.log("Tổng của mảng là: " + sum);
+    console.log("Số lẻ trong mảng là: " + oddCount);
+}
